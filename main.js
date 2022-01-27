812cb94bd129515f0bd3c9dc56fb995bc91f668d
aratrikajeet <86361439+aratrikajeet@users.noreply.github.com>
2022-01-27 10:27:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+score=0;
+function updateScore()
+{
+
+    score = score +1;
+    document.getElementById("score").innerHTML="Score:" + score;
+}
+
+function SaveScore()
+{
+    localStorage.setItem("score",score);
+}
+function nextPage()
+{
+    window.location="activity_2.html";
+}
